18624c39c9bd0e4c4338e4a07604e0771c722100
hyeinkim1305 <haileyyy1305@gmail.com>
2021-01-26 11:27:57 +0900

    21.01.26_sol

---
+++ Bronze/2750번/2750.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+
+num = []
+
+for i in range(n):
+    m = int(input())
+    num.append(m)
+
+num = sorted(num)
+for i in num:
+    print(i)
+
+
+
---
+++ Bronze/2750번/README.md
@@ -0,0 +1,7 @@
+# BOJ 2750번
+
+#### 문제출처 https://www.acmicpc.net/problem/2750 
+
+
+
+![image-20210126085407149](README.assets/image-20210126085407149.png)
