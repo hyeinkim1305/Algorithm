8da8ed7faa1b73dce097e7324d9618b1482e5f95
hyeinkim1305 <haileyyy1305@gmail.com>
2021-02-02 20:57:02 +0900

    ~21.02.01

--- SWEA/D2/2005번/2005.py
+++ SWEA/D2/2005번/2005.py
@@ -25,26 +25,4 @@ for t in range(T):
     print(' '.join(num))
 
       
-     
-
-
-
-
-
-
-
-
-
-
-#   for j in range(1, N-2):
-#     new_list = []
-#     new_list.append(1)
-#     new_list.append(ans[i][j-1] + ans[i][j])
-#     new_list.append(1)
-#     ans.append(new_list)
-
-# print(ans)
-
-
-
-# 양 옆에 1 추가 
+    
